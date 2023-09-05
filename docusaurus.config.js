e041166ec506e33804c6e72954fe05cf85db7089
wangjw <wangjw1210@gmail.com>
2023-09-05 20:07:01 +0900

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,12 +14,12 @@ const config = {
   url: 'https://wangjw.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/my-website/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'wangjw', // Usually your GitHub org/user name.
-  projectName: 'wangjw.github.io', // Usually your repo name.
+  projectName: 'my-website', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
